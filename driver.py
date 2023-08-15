b94c6d042b2c3542c54d99f5948267f808e9e877
Simon Stone <simon.stone@dartmouth.edu>
2023-08-15 14:42:00 -0400

    cleans up some unused files and comments in all tables

--- driver.py
+++ driver.py
@@ -36,14 +36,14 @@ def main (sourcefile, year, datadir, data_format):
     Keyp_list = []
     Conf_list = []
     CoSp_list = []
-    
+
     with open(args.sourcefile, 'r') as data:
 
         while True:
-        
+
             count  += 1
             record  = rr.get_record(data)
-            
+
             if not record:
                 logging.debug("Completed processing {0}".format(sourcefile))
                 print("Processed {0} records".format(count-1))
@@ -53,7 +53,7 @@ def main (sourcefile, year, datadir, data_format):
                 #context = extract.load_data(record)
                 REC    = ET.fromstring(record)
                 wos_id = list(REC.iterfind('UID'))[0].text
-            
+
                 Pub, Languages, Headings, Subheadings, Subjects    = x.extract_pub_info(wos_id, REC)
                 Pubs_list.extend(Pub)
                 Lang_list.extend(Languages)
@@ -61,53 +61,51 @@ def main (sourcefile, year, datadir, data_format):
                 Subh_list.extend(Subheadings)
                 Subj_list.extend(Subjects)
 
-                """
                 #print pub
                 Publishers = x.extract_publisher(wos_id, REC)
                 Publ_list.extend(Publishers)
-            
-            
+
+
                 #print publisher
                 Authors   = x.extract_authors(wos_id, REC)
                 Auth_list.extend(Authors)
-            
+
                 ##print authors
                 Institutions, Name_inst_relation = x.extract_addresses(wos_id, REC)
                 Inst_list.extend(Institutions)
                 NaIn_list.extend(Name_inst_relation)
                 #print len(Inst_list), Institutions
                 #print len(NaIn_list), Name_inst_relation
-                
+
                 Editions = x.extract_editions(wos_id, REC)
                 Edit_list.extend(Editions)
 
                 References = x.extract_references(wos_id, REC)
                 Refs_list.extend(References)
-                                
+
                 Ftext, Funding = x.extract_funding(wos_id, REC)
                 Ftxt_list.extend(Ftext)
                 Fund_list.extend(Funding)
-                
+
                 Conf, Sponsor  = x.extract_conferences(wos_id, REC)
                 Conf_list.extend(Conf)
                 CoSp_list.extend(Sponsor)
                 #print len(Conf_list), Conf
                 #print len(CoSp_list), Sponsor
-            
+
                 Keywords, Keywords_plus = x.extract_keywords(wos_id, REC)
                 Keyw_list.extend(Keywords)
                 Keyp_list.extend(Keywords_plus)
                 #print Keywords
-                """
+
             except Exception as e:
                 print("[ERROR:{0}] Caught an exception : {1}".format(wos_id, e))
                 pass
 
 
-                
+
     try :
         x.dump(Pubs_list,    h_source,       t_source,         year+'source',       '{0}/source.{1}'.format(datadir, data_format),     data_format=data_format)
-        """
         x.dump(Edit_list,    h_editions,       t_editions,         year+'editions',       '{0}/editions.{1}'.format(datadir, data_format),     data_format=data_format)
         x.dump(Ftxt_list,    h_fundingtexts,   t_fundingtexts,     year+'fundingtext',    '{0}/fundingtext.{1}'.format(datadir, data_format),  data_format=data_format)
         x.dump(Fund_list,    h_funding,        t_funding,          year+'funding',        '{0}/funding.{1}'.format(datadir, data_format),      data_format=data_format)
@@ -125,18 +123,18 @@ def main (sourcefile, year, datadir, data_format):
         x.dump(Auth_list,    h_contributors,   t_contributors,     year+'contributors',   '{0}/contributors.{1}'.format(datadir, data_format), data_format=data_format)
         x.dump(Inst_list,    h_institutions,   t_institutions,     year+'institutions',   '{0}/institutions.{1}'.format(datadir, data_format), data_format=data_format)
         x.dump(NaIn_list,    h_name_inst,      t_name_inst,        year+'affiliations',   '{0}/affiliations.{1}'.format(datadir, data_format), data_format=data_format)
-        """
+
     except Exception as e:
         print("[ERROR] Dumping failed for {0}".format(sourcefile))
         logging.error("[ERROR] Dumping failed for {0}".format(sourcefile))
         exit(-1)
-        
+
     return
 
 
 
 if __name__ == "__main__" :
-    
+
     parser   = argparse.ArgumentParser()
     parser.add_argument("-s", "--sourcefile", default="sample.xml", help="Path to data file")
     parser.add_argument("-v", "--verbosity", default="DEBUG", help="set level of verbosity, DEBUG, INFO, WARN")
@@ -146,7 +144,7 @@ if __name__ == "__main__" :
     args   = parser.parse_args()
 
     print("Processing : {0}".format(args.sourcefile))
-    
+
     logging.basicConfig(filename=args.logfile, level=log_levels[args.verbosity],
                         format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
                         datefmt='%m-%d %H:%M')
@@ -164,6 +162,5 @@ if __name__ == "__main__" :
         print(args.sourcefile.replace('.xml', ''))
 
     print("[DEBUG] Processing year : {0}".format(year))
-    
+
     main(args.sourcefile, year, args.dir, args.format)
-    
